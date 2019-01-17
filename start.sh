fef62b1bb953131010147e4dfee069361cef789e
ScenesK <scenesk.ngt@gmail.com>
2019-01-17 18:40:36 +0900

    add notebook.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 docker start dslec
-docker exec dslec nohup bash /root/workspace/python/notebook.sh > out.log 2> err.log &
+docker exec dslec nohup bash /root/workspace/notebook.sh > out.log 2> err.log &
 echo 'Access 127.0.0.1:8888 for Jupyter (pass:jupyter)'

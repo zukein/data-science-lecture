37830689538517ff90e4cc2a65a334947992fdfb
ScenesK <scenesk.ngt@gmail.com>
2019-01-29 23:46:01 +0900

    Change notebook.sh permission

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 docker start dslec
+chmod 777 workspace/notebook.sh
 docker exec dslec nohup /root/workspace/notebook.sh > out.log 2> err.log &
 echo 'Access 127.0.0.1:8888 for Jupyter (pass:jupyter)'

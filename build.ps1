a1db5770e9102e240ecdc107bfb5fcf643cb28fc
ScenesK <scenesk.ngt@gmail.com>
2019-08-18 13:42:54 +0900

    Modify ps1 files

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 . ".\config.ps1"
 
 docker build -t $IMAGE .
-docker run --name $CONTAINER -p ${JUPYTER_PORT}:8888 -p ${TENSOR_BOARD_PORT}:6006 -v $pwd/workspace:/root/workspace -tid $IMAGE bash
+docker run --name $CONTAINER -p ${JUPYTER_PORT}:8888 -p ${TENSOR_BOARD_PORT}:6006 -v ${PWD}/workspace:/root/workspace -tid $IMAGE bash
 docker exec $CONTAINER pip freeze > requirements.txt
 docker stop $CONTAINER
